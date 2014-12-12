e013b8646a7852106de3b8f58b6117196972c0a6
Julian Skinner <j.skinner@arpc.com>
2014-12-12 11:34:10 -0500

    just to revert previous hackery file.

--- test/features/hackery_test.rb
+++ test/features/hackery_test.rb
@@ -2,18 +2,17 @@ require 'test_helper'
 
 describe 'hackery' do
 
-  it 'will upload file', :filename => __NAME__ do
-    false.must_equal :true
-    # visit '/'
-    # fill_in 'Email', with: 'bob.pack@arpc.com'
-    # fill_in 'Password', with: 'yagni123'
-    # click_link_or_button 'Sign in'
-    #
-    # find(:css, '[rel="bulk-upload-button"]').click
-    #
-    # script = %{$('#batch_document').after('<input type="file" id="random_file" />');}
-    #
-    # page.execute_script(script)
-    # sleep(200)
+  it 'will upload file' do
+    visit '/'
+    fill_in 'Email', with: 'bob.pack@arpc.com'
+    fill_in 'Password', with: 'yagni123'
+    click_link_or_button 'Sign in'
+
+    find(:css, '[rel="bulk-upload-button"]').click
+
+    script = %{$('#batch_document').after('<input type="file" id="random_file" />');}
+
+    page.execute_script(script)
+    sleep(200)
   end
 end
