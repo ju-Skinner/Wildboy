0a47a02cde6341d52b652c8c92786fde7e80ff20
Julian Skinner <j.skinner@arpc.com>
2014-12-12 09:12:41 -0500

    WIP - need to still get file and line number information.

--- test/features/metadata_test.rb
+++ test/features/metadata_test.rb
@@ -1,14 +1,19 @@
 require "test_helper"
 
-class MetadataTest < AcceptanceSpec
-  describe "Capybara" do
+describe "Capybara" do
 
-    it 'should use poltergeist with js metadata', :js => true do
-      Capybara.current_driver.must_equal :poltergeist
-    end
+  it 'should use poltergeist with js metadata', :js => true do
+    Capybara.current_driver.must_equal :poltergeist
+  end
 
-    it 'should use selenium without js metadata' do
-      Capybara.current_driver.must_equal :selenium
+  it 'should use selenium without js metadata' do
+    Capybara.current_driver.must_equal :selenium
+  end
+
+  describe 'HTML' do
+    it 'should save and open page on failure' do
+      visit '/'
+      page.must_have_content 'Welcome Julian Skinner'      
     end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,20 +9,21 @@ require 'capybara/poltergeist'
 require 'pry'
 
 Minitest::Reporters.use!(
-  Minitest::Reporters::SpecReporter.new(:color => true),
-  ENV,
-  Minitest.backtrace_filter
+  Minitest::Reporters::SpecReporter.new(:color => true)
+#   ,ENV
+#   ,Minitest.backtrace_filter
 )
 
 # Capybara Configuration
 Capybara.configure do |config|
   config.run_server = false
-  config.app_host = 'http://localhost:3000'
+  config.app_host = 'http://localhost:3000' #'http://acceptance.iclaimsonline.com'
+  config.asset_host = config.app_host
   config.register_driver :selenium do |app|
     Capybara::Selenium::Driver.new(app, :browser => :firefox)
   end
 
-  # config.save_and_open_page_path = File.dirname(__FILE__) + '/../snapshots'
+  config.save_and_open_page_path = Pathname.new(File.expand_path "failures")
   config.default_driver = :selenium
   config.javascript_driver = :poltergeist
 end
@@ -31,7 +32,7 @@ end
 require "minitest/pride"
 
 # for minitest/spec
-class AcceptanceSpec < Minitest::Spec
+class Minitest::Spec
   include Capybara::DSL
 
   before do
@@ -42,8 +43,26 @@ class AcceptanceSpec < Minitest::Spec
     end
   end
 
-  def teardown
+  after  do
+    if ['F', 'E'].include?(result_code)
+      puts "Passed: #{passed?}, Error: #{error?}, Failed: #{failures.size}"
+
+      # filename = File.basename(meta[:file_path])
+      # line_number = meta[:line_number]
+      # screenshot_name = "screenshot-#{filename}-#{line_number}.png"
+      # screenshot_name = "screenshot.png"
+      # path = Pathname.new(File.expand_path "test/failures")
+      # screenshot_path = "#{path}/#{screenshot_name}"
+      puts metadata
+      result = page.save_page
+
+      puts "\n Screenshot: #{result}"
+    end
+
     Capybara.reset_session!
     Capybara.use_default_driver
   end
+  # def teardown
+  #
+  # end
 end
